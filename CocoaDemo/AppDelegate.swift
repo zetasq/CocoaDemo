b7e267a99709a10da1de5a2cde29b73222e86884
Zhu Shengqi <shengqi@teambition.com>
2017-08-23 15:51:42 +0800

    Initial Commit

---
+++ CocoaDemo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CocoaDemo
+//
+//  Created by Zhu Shengqi on 23/08/2017.
+//  Copyright © 2017 zetasq. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
